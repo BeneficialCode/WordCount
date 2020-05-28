ad571487c57480fb3f36c98ddba6b28122861748
BeneficialCode <3123132899@qq.com>
2020-05-28 19:24:35 +0800

    Hello

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -1,20 +1,45 @@
-﻿// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#include <iostream>
+int main(int argc, char* argv[]) {
+	int n, m = 0;
+	FILE *fp = NULL;
+	errno_t err;
+	char c;
+	if (argc != 3) {
+		printf("parameter error!\n");
+		exit(0);
+	}
+	err = fopen_s(&fp, argv[2], "rb");
+	if (err) {
+		printf("can not open the file\n");
+		exit(0);
+	}
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	if (!strcmp("-c", argv[1])) {
+		fseek(fp, 0, SEEK_END);
+		n = ftell(fp);
+		printf("字符数：%d\n", n);
+	} else if (!strcmp("-w", argv[1])) {
+		fseek(fp, 0, SEEK_SET);
+		while (!feof(fp)) {
+			c = fgetc(fp);
+			if (c == ',' || c == ' ') {
+				do {
+					c = fgetc(fp);
+					if (c == ',' || c == ' ')
+						continue;
+					else {
+						if (!feof(fp))
+							m++;
+						break;
+					}
+				} while (1);
+			}
+		}
+		printf("单词数：%d\n", m);
+	}
+	fclose(fp);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
